e2e4945cc8278b934983417ee491ea87ec2e1250
janireMartinez <24dm.janire.martinez@arangoya.net>
2026-02-05 22:38:30 +0100

    Logo janire/jose

--- PingMe2.0/src/main/java/conexionFrontend/MensajeCallback.java
+++ PingMe2.0/src/main/java/conexionFrontend/MensajeCallback.java
@@ -2,5 +2,5 @@ package conexionFrontend;
 
 // Interfaz de callback para recibir mensajes del servidor
 public interface MensajeCallback {
-    void onMensajeRecibido(String mensaje);
+    void onMensajeRecibido(String mensaje); // Método que se llamará cuando se reciba un mensaje del servidor
 }
